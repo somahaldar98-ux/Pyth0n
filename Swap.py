84e459c8b290981a597245ef0c9bd510127c70b5
Error <somahaldar98@gmail.com>
2026-07-31 12:40:18 -0500

    Welcome

---
+++ Swap.py
@@ -0,0 +1,5 @@
+A=5
+B=10
+A,B = B,A
+print("A=",A)
+print("B=",B)
---
+++ Welcome.py
@@ -0,0 +1,9 @@
+print("Welcome to the world of programming!")
+# Printing a number
+print(5)
+# Next line after print
+print("k fyrty gvfyudgyugyugergfvgyu \n" )
+# End arguement for all print statement
+print("Hello python", end="*")
+# Past multiple arguements
+print("Hello", 5)
