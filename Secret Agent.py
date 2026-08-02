b6f29e11e824c8e0f41f8ec0143ff70b3ee9a9b7
Error <somahaldar98@gmail.com>
2026-08-02 14:20:12 -0500

    Data types

---
+++ Secret Agent.py
@@ -0,0 +1,13 @@
+name=input("Enter your real name")
+gadget=input("Enter your favorite gadget:")
+
+agent_number = 7
+speed_rating = 9.5
+mission_count = 12
+height_m = 1.65
+is_active = True
+print("Name:",name,type ("name")) 
+print("gadget", gadget, type("gadget"))
+print("agent_number", agent_number, type(agent_number))
+print("speed_rating",speed_rating, type(speed_rating))
+print("is_active",is_active, type(is_active))
